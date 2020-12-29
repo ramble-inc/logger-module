24bd6b2188fefa5226c319677e88549ba170c727
mizozobu <hamc.m.so.j12@gmail.com>
2020-12-29 18:04:30 +0900

    fix tsconfig for build

--- test/jest-e2e.setup.afterenv.ts
+++ test/jest-e2e.setup.afterenv.ts
@@ -3,29 +3,3 @@
  * Default: 5000
  */
 jest.setTimeout(10000);
-
-/**
- * Mock winston to silence logger output
- */
-// jest.mock('winston', () => ({
-//   format: {
-//     colorize: jest.fn(),
-//     combine: jest.fn(),
-//     json: jest.fn(),
-//     label: jest.fn(),
-//     timestamp: jest.fn(),
-//     printf: jest.fn(),
-//   },
-//   createLogger: jest.fn().mockReturnValue({
-//     error: jest.fn(),
-//     warn: jest.fn(),
-//     info: jest.fn(),
-//     http: jest.fn(),
-//     verbose: jest.fn(),
-//     debug: jest.fn(),
-//     silly: jest.fn(),
-//   }),
-//   transports: {
-//     Console: jest.fn(),
-//   },
-// }));
